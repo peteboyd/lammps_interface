2ae2a5a4b0590ccf9f100761c000ac2b2bb0ced9
Peter Boyd <peter.g.boyd@gmail.com>
2015-11-18 03:27:25 -0800

    Changing how lammps_potentials interfaces with the program

--- lammps_potentials.py
+++ lammps_potentials.py
@@ -1,83 +1,382 @@
 #!/usr/bin/env python
-from datetime import date
-import numpy as np
-import math
-import itertools
-from atomic import MASS, ATOMIC_NUMBER
-from ccdc import CCDC_BOND_ORDERS
-DEG2RAD=np.pi/180.
 
-class Bond_Pot(object):
+class BondPotential(object):
     """
     Class to hold bond styles that are implemented in lammps
     Purpose is to store info that the user wants to use to overwrite standard UFF output of lammps_interface
     """
     
+    class Class2(object):
+        """Potential defined as
 
-    def __init__(self):
-        self.name = None
-        self.style = 0
-        self.choices = {'none': 0, 'class2': 1, 'fene': 2, 'fene/expand': 3, 'harmonic': 4, 'morse': 5, 'nonlinear': 6, 'quartic': 7, 'table': 8}
-		self.num_params = {'none': 0, 'class2': 0, 'fene': 0, 'fene/expand': 0, 'harmonic': 2, 'morse': 0, 'nonlinear': 0, 'quartic': 0, 'table': 0}
-        self.params = []
+        E = K2*(r-R0)^2 + K3*(r-R0)^3 + K4*(r-R0)^4
 
+        Input parameters: R0, K2, K3, K4
+        """
+        def __init__(self):
+            self.name = "class2"
+            self.R0 = 0.
+            self.K2 = 0.
+            self.K3 = 0.
+            self.K4 = 0.
 
-    def set_type(string):
-        self.name = string
-        try:
-            self.style = self.choices[self.name]
-        except KeyError:
-            raise KeyError('No LAMMPS bond_style found matching: ' + str(self.name) + '\nPlease check your overwrite_potentials.txt file')
+        def __str__(self):
+            return ""
 
-        if self.num_params[self.name] == 0:
-            raise NameError('Sorry, bond_style <' + str(self.name) + '> not implemented yet.\nPlease contact authors in README.txt')
+    class Fene(object):
+        """Potential defined as
 
+        E = -0.5*K*R0^2 * ln[1-(r/R0)^2] + 4*eps*[(sig/r)^12 - (sig/r)^6] + eps
 
-    def set_params(inlist):
-        if self.name == None:
-            raise NameError('Potential parameters assigned before potential type')
-        else:
-            if len(inlist) != self.num_params:
-                raise NameError('Incorrect num of potential parameters given (=' + str(len(inlist)) + ').  Potential of type ' + str(self.name) | 'requires ' + str(self.num_params[self.name]))
-            else:
-               self.params = inlist
+        Input parameters: K, R0, eps, sig
+        """
 
+        def __init__(self):
+            self.name = "fene" # NB: fene/omp and fene/kk exist
+            self.K = 0.
+            self.R0 = 0.
+            self.eps = 0.
+            self.sig = 0.
 
+        def __str__(self):
+            return ""
 
-class Angle_Pot(object) 
+    class FeneExpand(object):
+        """Potential defined as
+
+        E = -0.5*K*R0^2 * ln[1-(r-del/R0)^2] + 
+               4*eps*[(sig/r-del)^12 - (sig/r-del)^6] + eps
+
+        Input parameters: K, R0, eps, sig, del
+        """
+        def __init__(self):
+            self.name = "fene/expand" # NB: fene/expand/omp exists
+            self.K = 0.
+            self.R0 = 0.
+            self.eps = 0.
+            self.sig = 0.
+            self.del = 0.
+
+        def __str__(self):
+            return ""
+        
+    class Harmonic(object):
+        """Potential defined as
+
+        E = K*(r - R0)^2
+
+        Input parameters: K, R0
+        """
+
+        def __init__(self):
+            self.name = "harmonic" # harmonic/kk and harmonic/omp exist
+            self.K = 0.
+            self.R0 = 0.
+
+        def __str__(self):
+            return ""
+
+    class Morse(object):
+        """Potential defined as
+
+        E = D*[1 - e^(-alpha*(r-R0))]^2
+
+        Input parameters: D, alpha, R0
+        """
+        def __init__(self):
+            self.name = "morse" # morse/omp exists
+            self.K = 0.
+            self.R0 = 0.
+
+        def __str__(self):
+            return ""
+
+    class NonLinear(object):
+        """Potential defined as
+
+        E = eps*(r-R0)^2 / [lamb^2 - (r-R0)^2] 
+
+        Input parameters: eps, R0, lamb
+        """
+        def __init__(self):
+            self.name = "nonlinear" # nonlinear/omp exists
+            self.eps = 0.
+            self.R0 = 0.
+            self.lamb = 0.
+
+        def __str__(self):
+            return ""
+
+    class Quartic(object):
+        """Potential defined as
+
+        E = K*(r-Rc)^2 * (r - Rc - B1) * (r - Rc - B2) + U0 + 
+                  4*eps*[(sig/r)^12 - (sig/r)^6] + eps
+
+        Input parameters: K, B1, B2, Rc, U0
+        """
+        def __init__(self):
+            self.name = "quartic" # quartic/omp exists
+            self.K = 0.
+            self.B1 = 0.
+            self.B2 = 0.
+            self.Rc = 0.
+            self.U0 = 0.
+
+        def __str__(self):
+            return ""
+
+    class Table(object):
+        """Potential read from file."""
+        raise NotImplementedError ("Have not implemented the table funtion for lammps yet.")
+
+    class HarmonicShift(object):
+        """Potential defined as
+
+        E = Umin/(R0 - Rc)^2 * [(r-R0)^2 - (Rc - R0)^2] 
+
+        Input parameters: Umin, R0, Rc 
+        """
+        def __init__(self):
+            self.name = "harmonic/shift" # harmonic/shift/omp exists
+            self.Umin = 0.
+            self.R0 = 0.
+            self.Rc = 0.
+
+        def __str__(self):
+            return ""
+
+    class HarmonicShiftCut(object):
+        """Potential defined as
+
+        E = Umin/(R0 - Rc)^2 * [(r-R0)^2 - (Rc - R0)^2] 
+
+        Input parameters: Umin, R0, Rc 
+        """
+        def __init__(self):
+            self.name = "harmonic/shift/cut" # harmonic/shift/cut/omp exists
+            self.Umin = 0.
+            self.R0 = 0.
+            self.Rc = 0.
+
+        def __str__(self):
+            return ""
+
+class AnglePotential(object) 
     """
     Class to hold angle styles that are implemented in lammps
-    Purpose is to store info that the user wants to use to overwrite standard UFF output of lammps_interface
     """
+    
+    class Charmm(object):
+        """Potential defined as
 
-    def __init__(self):
-        self.name = None
-        self.style = 0
-        self.choices = {'none': 0, 'charmm': 1, 'class2': 2, 'cosine': 3, 'cosine/delta': 4, 'cosine/periodic': 5, 'cosine/squared': 6, 'harmonic': 7, 'table': 8, 'fourier': 9, 'fourier/simple': 10}
-        self.num_params = {'none': 0, 'charmm': 0, 'class2': 0, 'cosine': 0, 'cosine/delta': 0, 'cosine/periodic': 0, 'cosine/squared': 0, 'harmonic': 2, 'table': 0}
-        self.params = []
+        E = K*(theta - theta0)^2 + Kub*(r - Rub)^2
 
+        Input parameters: K, theta0, Kub, Rub
+        """
+        def __init__(self):
+            self.name = "charmm" # charmm/kk and charmm/omp exist
+            self.K = 0.
+            self.theta0 = 0.
+            self.Kub = 0.
+            self.Rub = 0.
 
-    def set_type(string):
-        self.name = string
-        try:
-            self.style = self.choices[self.name]
-        except KeyError:
-            raise KeyError('No LAMMPS angle_style found matching: ' + str(self.name) + '\nPlease check your overwrite_potentials.txt file')
+        def __str__(self):
+            return ""
 
-        if self.num_params[self.name] == 0:
-            raise NameError('Sorry, angle_style <' + str(self.name) + '> not implemented yet.\nPlease contact authors in README.txt')
+    class Class2(object):
+        raise NotImplementedError ("Will get on this..")
 
-    def set_params(inlist):
-        if self.name == None:
-            raise NameError('Potential parameters assigned before potential type')
-        else:
-            if len(inlist) != self.num_params:
-                raise NameError('Incorrect num of potential parameters given (=' + str(len(inlist)) + ').  Potential of type ' + str(self.name) | 'requires ' + str(self.num_params[self.name]))
-            else:
-               self.params = inlist
+    class Cosine(object):
+        """Potential defined as
 
+        E = K*[1 - cos(theta)] 
 
+        Input parameters: K
+        """
+        def __init__(self):
+            self.name = "cosine" # cosine/omp exists 
+            self.K = 0.
+
+        def __str__(self):
+            return ""
+
+    class CosineDelta(object):
+        """Potential defined as
+
+        E = K*[1 - cos(theta-theta0)] 
+
+        Input parameters: K, theta0
+        """
+        def __init__(self):
+            self.name = "cosine/delta" # cosine/delta/omp exists 
+            self.K = 0.
+            self.theta0 = 0.
+
+        def __str__(self):
+            return ""
+
+    class CosinePeriodic(object):
+        """Potential defined as
+
+        E = C*[1 - B*(-1)^n*cos(n*theta)] 
+
+        Input parameters: C, B, n
+        """
+        def __init__(self):
+            self.name = "cosine/periodic" # cosine/periodic/omp exists 
+            self.C = 0.
+            self.B = 0
+            self.n = 0
+
+        def __str__(self):
+            return ""
+
+    class CosineSquared(object):
+        """Potential defined as
+
+        E = K*[cos(theta) - cos(theta0)]^2
+
+        Input parameters: K, theta0
+        """
+        def __init__(self):
+            self.name = "cosine/squared" # cosine/squared/omp exists 
+            self.K = 0.
+            self.theta0 = 0.
+
+        def __str__(self):
+            return ""
+
+    class Harmonic(object):
+        """Potential defined as
+
+        E = K*(theta - theta0)^2
+
+        Input parameters: K, theta0
+        """
+        def __init__(self):
+            self.name = "harmonic" # harmonic/kk and harmonic/omp exist 
+            self.K = 0.
+            self.theta0 = 0.
+
+        def __str__(self):
+            return ""
+
+    class Table(object):
+        raise NotImplementedError ("Have not implemented the table funtion for lammps yet.")
+
+    class CosineShift(object):
+        """Potential defined as
+
+        E = -Umin/2 * [1 + cos(theta - theta0)]
+
+        Input parameters: Umin, theta0
+        """
+        def __init__(self):
+            self.name = "cosine/shift" # cosine/shift/omp exists 
+            self.Umin = 0.
+            self.theta0 = 0.
+
+        def __str__(self):
+            return ""
+    
+    class CosineShiftExp(object):
+        """Potential defined as
+
+        E = -Umin * [e^{-a*U(theta,theta0)} - 1] / [e^a - 1]
+
+        where U(theta,theta0) = -0.5*(1 + cos(theta - theta0))
+
+        Input parameters: Umin, theta0, a
+        """
+        def __init__(self):
+            self.name = "cosine/shift/exp" # cosine/shift/exp/omp exists 
+            self.Umin = 0.
+            self.theta0 = 0.
+            self.a = 0.
+
+        def __str__(self):
+            return ""
+
+    class Dipole(object):
+        """Potential defined as
+
+        E = K*(cos(gamma) - cos(gamma0))^2 
+
+        Input parameters: K, gamma0
+        """
+        def __init__(self):
+            self.name = "dipole" # dipole/omp exists 
+            self.K = 0.
+            self.gamma0 = 0.
+
+        def __str__(self):
+            return ""
+    
+    class Fourier(object):
+        """Potential defined as
+
+        E = K*[C0 + C1*cos(theta) + C2*cos(2*theta)]
+
+        Input parameters: K, C0, C1, C2
+        """
+        def __init__(self):
+            self.name = "fourier" # fourier/omp exists 
+            self.K = 0.
+            self.C0 = 0.
+            self.C1 = 0.
+            self.C2 = 0.
+
+        def __str__(self):
+            return ""
+
+    class FourierSimple(object):
+        """Potential defined as
+
+        E = K*[1 + c*cos(n*theta)]
+
+        Input parameters: K, c, n
+        """
+        def __init__(self):
+            self.name = "fourier/simple" # fourier/simple/omp exists 
+            self.K = 0.
+            self.c = 0.
+            self.n = 0.
+
+        def __str__(self):
+            return ""
+
+    class Quartic(object):
+        """Potential defined as
+
+        E = K2*(theta - theta0)^2 + K3*(theta - theta0)^3 + K4(theta - theta0)^4
+
+        Input parameters: theta0, K2, K3, K4
+        """
+        def __init__(self):
+            self.name = "quartic" # quartic/omp exists 
+            self.theta0 = 0.
+            self.K2 = 0.
+            self.K3 = 0.
+            self.K4 = 0.
+
+        def __str__(self):
+            return ""
+    
+    class Sdk(object):
+        """Potential defined as
+
+        E = K*(theta - theta0)^2 
+
+        Input parameters: K, theta0 
+        """
+        def __init__(self):
+            self.name = "sdk" 
+            self.K = 0.
+            self.theta0 = 0.
+
+        def __str__(self):
+            return ""
 
 class Dihedral_Pot(object) 
     """
