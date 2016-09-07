518380d7e59467bdd45172426c5dd7e4fa0358a6
Peter Boyd <peter.g.boyd@gmail.com>
2016-09-07 07:34:03 -0700

    commented out special_bonds for UFF and UFF4MOF to avoid double counting of aromatic atoms

--- ForceFields.py
+++ ForceFields.py
@@ -2451,7 +2451,7 @@ class UFF(ForceField):
     
     def special_commands(self):
         st = ["%-15s %s %s"%("pair_modify", "tail yes", "mix arithmetic"), 
-              "%-15s %s"%("special_bonds", "lj/coul 0.0 0.0 1.0"), 
+              #"%-15s %s"%("special_bonds", "lj/coul 0.0 0.0 1.0"), 
               "%-15s %.1f"%('dielectric', 1.0)
               ] 
         return st
@@ -3441,7 +3441,7 @@ class UFF4MOF(ForceField):
     
     def special_commands(self):
         st = ["%-15s %s %s"%("pair_modify", "tail yes", "mix arithmetic"),
-              "%-15s %s"%("special_bonds", "lj/coul 0.0 0.0 1.0"),
+              #"%-15s %s"%("special_bonds", "lj/coul 0.0 0.0 1.0"),
               "%-15s %.1f"%('dielectric', 1.0)
               ] 
         return st
