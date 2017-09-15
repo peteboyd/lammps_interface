1cc8eca38bd07be1b824d6e17ad6c74518018a4a
kbsezginel <kbs37@pitt.edu>
2017-09-15 14:41:24 -0400

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="lammps_interface",
+    version="0.1",
+    description="Automatic generation of LAMMPS input files for molecular dynamics simulations of MOFs",
+    install_requires=requirements,
+    include_package_data=True,
+    packages=find_packages()
+)
