849f70cada2b5a6b621dfa912d70826b4eda735b
Peter Boyd <peter.g.boyd@gmail.com>
2019-09-13 19:20:47 -0400

    removed molecule deposit features

--- lammps_interface/InputHandler.py
+++ lammps_interface/InputHandler.py
@@ -283,34 +283,6 @@ class Options(object):
                                           "Default is 200,000 steps. (corresponding to "+
                                           "200 ps if the timestep is 1 fs)")
 
-        molecule_insertion_group = parser.add_argument_group("Molecule insertion options")
-        molecule_insertion_group.add_argument("--insert-molecule",
-                                              action="store",
-                                              type=str,
-                                              default="",
-                                              dest="insert_molecule",
-                                              help="Prepeare an insertion of this molecule type."+
-                                                   " Default is no molecule insertion. Current options are "+
-                                                   "TIP5P_Water, TIP4P_Water, SPC_E, TIP3P."+
-                                                   " More to come ;)")
-
-        molecule_insertion_group.add_argument("--deposit",
-                                              action="store",
-                                              type=int,
-                                              default=0,
-                                              dest="deposit",
-                                              help="Create commands to place DEPOSIT particles "+
-                                                   "in the region of the unit cell. The particle "+
-                                                   "types are provided by the INSERT_MOLECULE value."+
-                                                   " This will also depend on the number of "+
-                                                   "equilibrium steps NEQSTP requested, and "+
-                                                   "currently only works with an NVT simulation."+
-                                                   " NOTE: The number of particles to deposit "+
-                                                   "is considered in the unit cell! The program "+
-                                                   "will multipy this value by the number of unit "+
-                                                   "cells that were needed to produce the simulation "+
-                                                   "supercell.")
-
         parser.add_argument(metavar="CIF", dest="cif_file",
                             help="path to cif file to interpret")
 
